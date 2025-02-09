2d42f8c7a1deb53fab648a021037faade4ff70da
Aulia Nur Fitriani <113015906+Auliaafitriani@users.noreply.github.com>
2025-02-10 00:21:26 +0700

    61app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -197,7 +197,7 @@ def main():
             <div style='text-align: center;'>
                 <img src="https://raw.githubusercontent.com/Auliaafitriani/SkripsiAulia/main/LogoPriorityAid.png" 
                      alt="Logo" 
-                     width="70" 
+                     width="100" 
                      style="margin-top: 0;">
             </div>
             """,
